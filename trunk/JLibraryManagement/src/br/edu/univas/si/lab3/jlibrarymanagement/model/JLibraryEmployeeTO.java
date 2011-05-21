9ff0953a462e1ea1394147d142accf17096c8544
mausrdr@gmail.com <mausrdr@gmail.com@afb8449f-ac50-22ad-b61a-9d9adf0c0adb>
2011-05-21 21:00:05 +0000

    - implemented of JLibraryEmployeeTO

--- trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryEmployeeTO.java
+++ trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryEmployeeTO.java
@@ -7,5 +7,109 @@ package br.edu.univas.si.lab3.jlibrarymanagement.model;
  *
  */
 public class JLibraryEmployeeTO {
-
+	private Integer id;
+	private String name;
+	private String address;
+	private String email;
+	private String phone;
+	private String cpf;
+	private String password;
+	private Boolean manager;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return cpf;
+	}
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the manager
+	 */
+	public Boolean getManager() {
+		return manager;
+	}
+	/**
+	 * @param manager the manager to set
+	 */
+	public void setManager(Boolean manager) {
+		this.manager = manager;
+	}
+	
 }
