5a3c13c3a73025ded4a01b8eda8250cfc1db5542
luannfaria <luannfaria@afb8449f-ac50-22ad-b61a-9d9adf0c0adb>
2011-05-22 22:55:51 +0000

    - Create methods

--- trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/controller/JLibraryController.java
+++ trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/controller/JLibraryController.java
@@ -1,5 +1,21 @@
 package br.edu.univas.si.lab3.jlibrarymanagement.controller;
 
-public class JLibraryController {
+import br.edu.univas.si.lab3.jlibrarymanagement.model.JLibraryDAO;
+import br.edu.univas.si.lab3.jlibrarymanagement.model.JLibraryStudentTO;
+import br.edu.univas.si.lab3.jlibrarymanagement.view.JLibraryView;
 
+public class JLibraryController {
+	private JLibraryDAO dao;
+	private JLibraryView view;
+	
+	public JLibraryController(JLibraryDAO dao){
+		this.dao= dao;
+		this.view = new JLibraryView(this);
+	}
+	private void addNew(){
+		JLibraryStudentTO student = view.getStudent();
+		if(student != null){
+			dao.create(student);
+		}
+	}
 }
--- trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryRentBooksTO.java
+++ trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryRentBooksTO.java
@@ -1,11 +1,31 @@
 package br.edu.univas.si.lab3.jlibrarymanagement.model;
 
+import java.util.Date;
+
 public class JLibraryRentBooksTO {
 
 		private String bookName;
 		private String studentName;
 		private Integer bookId;
 		private Integer studentId;
+		private Boolean reserved;
+		private Date bookingdate;
+		private Date dateOfWithdrawal; 
+		private Boolean withdrawn;
+		
+	
+		public Date getDateOfWithdrawal() {
+			return dateOfWithdrawal;
+		}
+		public void setDateOfWithdrawal(Date dateOfWithdrawal) {
+			this.dateOfWithdrawal = dateOfWithdrawal;
+		}
+		public Boolean getWithdrawn() {
+			return withdrawn;
+		}
+		public void setWithdrawn(Boolean withdrawn) {
+			this.withdrawn = withdrawn;
+		}
 		public String getBookName() {
 			return bookName;
 		}
@@ -30,5 +50,19 @@ public class JLibraryRentBooksTO {
 		public void setStudentId(Integer studentId) {
 			this.studentId = studentId;
 		}
+		public Boolean getReserved() {
+			return reserved;
+		}
+		public void setReserved(Boolean reserved) {
+			this.reserved = reserved;
+		}
+		public Date getBookingdate() {
+			return bookingdate;
+		}
+		public void setBookingdate(Date bookingdate) {
+			this.bookingdate = bookingdate;
+		}
+		
+		
 		
 }
