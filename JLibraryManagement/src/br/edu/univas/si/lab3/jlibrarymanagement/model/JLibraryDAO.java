b51632743a561472ff6c83578bd9ab8d86295d58
luannfaria <luannfaria@afb8449f-ac50-22ad-b61a-9d9adf0c0adb>
2011-05-22 22:55:51 +0000

    - Create methods

--- JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryDAO.java
+++ JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryDAO.java
@@ -1,5 +1,15 @@
 package br.edu.univas.si.lab3.jlibrarymanagement.model;
 
-public class JLibraryDAO {
+import java.util.ArrayList;
 
+public class JLibraryDAO {
+	
+	private ArrayList<JLibraryStudentTO>studentList;
+	
+	public JLibraryDAO(){
+		studentList = new ArrayList<JLibraryStudentTO>();
+	}
+	public void create(JLibraryStudentTO student){
+		studentList.add(student); 
+	}
 }
