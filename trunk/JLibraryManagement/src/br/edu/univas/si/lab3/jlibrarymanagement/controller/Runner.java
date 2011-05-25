032dbb637e72d6a544d790fcc2e361aa9dfbc913
luannfaria <luannfaria@afb8449f-ac50-22ad-b61a-9d9adf0c0adb>
2011-05-25 22:56:31 +0000

    - implement initialize in maincontroller and runner.

--- trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/controller/Runner.java
+++ trunk/JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/controller/Runner.java
@@ -1,5 +1,11 @@
 package br.edu.univas.si.lab3.jlibrarymanagement.controller;
 
-public class Runner {
+import br.edu.univas.si.lab3.jlibrarymanagement.model.JLibraryEmployeeDAO;
+import br.edu.univas.si.lab3.jlibrarymanagement.model.JLibraryStudentDAO;
 
+public class Runner {
+	public static void main(String[] args) {
+		JLibraryMainController c = new JLibraryMainController(new JLibraryEmployeeDAO(),new JLibraryStudentDAO());
+		c.initialize();
+	}
 }
