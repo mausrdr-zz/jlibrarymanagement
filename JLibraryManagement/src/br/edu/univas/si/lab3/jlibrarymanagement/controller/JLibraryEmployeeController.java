dcd40c5c0f6fc4d38359cfb9cbd69d452bcba548
mausrdr@gmail.com <mausrdr@gmail.com@afb8449f-ac50-22ad-b61a-9d9adf0c0adb>
2011-05-25 03:06:11 +0000

    - Implement of Main Menu for Employee, Student Management Menu and delete student

--- JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/controller/JLibraryEmployeeController.java
+++ JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/controller/JLibraryEmployeeController.java
@@ -7,4 +7,77 @@ public class JLibraryEmployeeController {
 	
 	private JLibraryEmployeeDAO employeeDao;
 	private JLibraryEmployeeView employeeView;
+	
+	public JLibraryEmployeeController(JLibraryEmployeeDAO employeeDao) {
+		this.employeeDao = employeeDao;
+		this.employeeView = new JLibraryEmployeeView(this);
+	}
+	
+	public void employeeInitialize() {
+		employeeView.showEmployeeStudentMenu();
+	}
+	
+	public void menuEmployeeStudentChoice(int employeeStudentChoice) {
+		switch(employeeStudentChoice) {
+			case 1:
+				addNew();
+			break;
+			
+			case 2:
+				listAll();
+			break;
+			
+			case 3:
+				update();
+			break;
+			
+			case 4:
+				delete();
+			break;
+			
+			case 5:
+				employeeView.showEmployeeMainMenu();
+			break;
+		}
+	}
+
+	private void delete() {
+		String cpf = employeeView.getStudentCpf();
+		employeeDao.delete(cpf);
+	}
+
+	private void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void listAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addNew() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void menuEmployeeMainMenuChoice(int employeeMainMenuChoice) {
+		switch(employeeMainMenuChoice) {
+			
+			case 1:
+				employeeView.showEmployeeStudentMenu();
+			break;
+			
+			case 2:
+				employeeView.showEmployeeBookMenu();
+			break;
+			
+			case 3:
+				employeeView.showEmployeeRentMenu();
+			break;
+			
+			case 4:
+				employeeView.exitApp();
+		}
+	}
 }
--- JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryEmployeeDAO.java
+++ JLibraryManagement/src/br/edu/univas/si/lab3/jlibrarymanagement/model/JLibraryEmployeeDAO.java
@@ -4,12 +4,28 @@ import java.util.ArrayList;
 
 public class JLibraryEmployeeDAO {
 	private ArrayList<JLibraryEmployeeTO> employeeList;
+	private ArrayList<JLibraryStudentTO> studentList;
 	
 	public JLibraryEmployeeDAO() {
 		employeeList = new ArrayList<JLibraryEmployeeTO>();
+		studentList = new ArrayList<JLibraryStudentTO>();
 	}
 	
 	public void create(JLibraryEmployeeTO employee) {
 		employeeList.add(employee);
+	}
+
+	public void delete(String cpf) {
+		Integer index = 0, employeeStudentIndex = null;
+		for(JLibraryStudentTO student : studentList) {
+			if(student.getCpf().equalsIgnoreCase(cpf)) {
+				employeeStudentIndex = index;
+				break;
+			}
+			index ++;
+		}
+		if(employeeStudentIndex != null) {
+			studentList.remove(employeeStudentIndex);
+		}
 	}
 }
